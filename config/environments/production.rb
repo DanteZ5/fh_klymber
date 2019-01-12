341ec4ca29610255fd452782cd56a1460f67240f
François HAMEAU <f.hameau@gmail.com>
2019-01-12 14:40:41 +0100

    add .env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,6 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  # config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :letter_opener
+  config.active_record.dump_schema_after_migration = false
 
 end
