7802527a8bf9a522608ee7f092bc28bbe050b5a6
François HAMEAU <f.hameau@gmail.com>
2019-01-12 12:33:43 +0100

    ok

--- app/models/freelancer.rb
+++ app/models/freelancer.rb
@@ -2,7 +2,7 @@ class Freelancer < ApplicationRecord
   has_one :request
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\..*\z/ }
   validates :phone, presence: true, uniqueness: true, format: { with: /((\+)33|0|0033)[1-9](\d{2}){4}/ }
   validates :bio, presence: true, length: { minimum: 10 }
   validates :key, presence: true, uniqueness: true
